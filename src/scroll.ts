dcfb221c03d9ab7a534e5477ca8f10795f40ba8c
lauechev <laura.echeverri3@gmail.com>
2025-02-13 13:40:44 -0500

    👠 add scroll progress bar

--- src/scroll.ts
+++ src/scroll.ts
@@ -1,4 +1,6 @@
 const pixelsTag = document.querySelector<HTMLDivElement>('.pixels');
+const bodyTag = document.querySelector<HTMLElement>('body');
+const progressTag = document.querySelector<HTMLDivElement>('.progress');
 
 // When we scroll the page, update the pixels tag to be how far we've scrolled.
 
@@ -6,5 +8,16 @@ document.addEventListener('scroll', function () {
   const pixels = window.scrollY;
   if (pixelsTag) {
     pixelsTag.innerHTML = pixels.toString();
+  }
+});
+
+//when we scroll the page, make a progress bar that keeps track of the distance
+document.addEventListener('scroll', function () {
+  const pixels = window.scrollY;
+  if (bodyTag && progressTag) {
+    const pageHeight = bodyTag.getBoundingClientRect().height;
+    const totalScrollableDistance = pageHeight - window.innerHeight;
+    const percentage = pixels / totalScrollableDistance;
+    progressTag.style.width = `${100 * percentage}%`;
   }
 });
