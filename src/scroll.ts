423b4e60f01fcd1b29ad6f8cef7d126c8bc40d4e
lauechev <laura.echeverri3@gmail.com>
2025-02-16 13:35:10 -0500

    🦁 add scroll data attributes for title and page

--- src/scroll.ts
+++ src/scroll.ts
@@ -1,6 +1,9 @@
-const pixelsTag = document.querySelector<HTMLDivElement>('.pixels');
+const pixelsTag = document.querySelector<HTMLDivElement>('div.pixels');
 const bodyTag = document.querySelector<HTMLElement>('body');
-const progressTag = document.querySelector<HTMLDivElement>('.progress');
+const progressTag = document.querySelector<HTMLDivElement>('div.progress');
+const sectionsTag = document.querySelectorAll<HTMLElement>('section');
+const titleTag = document.querySelector<HTMLDivElement>('div.title');
+const pageTag = document.querySelector<HTMLDivElement>('div.page');
 
 // When we scroll the page, update the pixels tag to be how far we've scrolled.
 
@@ -20,4 +23,22 @@ document.addEventListener('scroll', function () {
     const percentage = pixels / totalScrollableDistance;
     progressTag.style.width = `${100 * percentage}%`;
   }
+});
+
+//when we scroll down the page, see how far down the page we scrolled
+//for each section, check whether we've passed it and if we have, updtae the text in the header
+
+document.addEventListener('scroll', function () {
+  const pixels = window.scrollY;
+
+  sectionsTag.forEach((section) => {
+    if (section.offsetTop - 100 <= pixels) {
+      if (titleTag && pageTag) {
+        const title = section.getAttribute('data-title') || '';
+        titleTag.innerHTML = title;
+        const page = section.getAttribute('data-page') || '';
+        pageTag.innerHTML = page;
+      }
+    }
+  });
 });
